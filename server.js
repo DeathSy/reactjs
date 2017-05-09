b86a849ce97e6388bf1b206710ce1bd2d42a9d88
kittipong <kittipong.sri@mail.kmutt.ac.th>
2017-05-10 05:39:01 +0700

    ADDED
    react boilerplate file
    index.js
    app.jsx
    home.jsx

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve('dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('express server is running on port', PORT);
+});
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    bundle: './src/components/index.js',
+    vendor: ['react', 'react-router-dom', 'jquery', 'bootstrap']
+  },
+  output: {
+    path: path.resolve('./dist'),
+    publicPath: 'http://localhost:3000',
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?)$/,
+        use: ['url-loader']
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: ['file-loader']
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: 'file-loader?name=images/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'js/vendor.js',
+      minChunks: 2
+    }),
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: 'index.html',
+      template: './src/views/index.html'
+    })
+  ],
+}
